abb4b562248399e64f0a8e305547d73aa22befcb
yochidros <mm9.movement.trb@gmail.com>
2019-07-29 19:06:14 +0900

    feat: realm.

---
+++ Data/DataStore/ClothesList/ClothesListDataStore.swift
@@ -0,0 +1,40 @@
+//
+//  ClothesListDataStore.swift
+//  Data
+//
+//  Created by Yoshiki Miyazawa on 2019/07/26.
+//  Copyright © 2019 yochidros. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+import Realm
+
+public protocol ClothesListDataStore {
+    func getTabItems(onResult: @escaping (Result<[TabEntity], DataStoreError>) -> Void)
+}
+
+public class ClothesListDataStoreImpl: ClothesListDataStore {
+    public func getTabItems(onResult: @escaping (Result<[TabEntity], DataStoreError>) -> Void) {
+        do {
+            let realm = try Realm()
+        } catch let e {
+           onResult(.failure(.unknown))
+        }
+        DispatchQueue.global().asyncAfter(deadline: .now() + .seconds(Int.random(in: 0...2))) { [onResult] in
+            let items = (0..<Int.random(in: 0..<20)).map({ TabEntity.init(name: "Tab \($0)", index: $0)})
+            guard items.count != 0 else {
+                onResult(.failure(.unknown))
+                return
+            }
+            onResult(.success(items))
+        }
+    }
+    public init() {
+    }
+}
+
+public enum DataStoreError: Error {
+    case unknown
+    case cast
+}
---
+++ Data/Manager/RealmManager.swift
@@ -0,0 +1,21 @@
+//
+//  RealmManager.swift
+//  Data
+//
+//  Created by Yoshiki Miyazawa on 2019/07/29.
+//  Copyright © 2019 yochidros. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class RealmManager {
+    static let shared: RealmManager = RealmManager()
+    
+    private init() {}
+}
+
+extension RealmManager {
+    func prepare() {
+    }
+}
